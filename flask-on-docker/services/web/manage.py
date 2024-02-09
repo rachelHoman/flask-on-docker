ba79c03c267e6843913db24a8bee93c1a7b87971
rachelHoman <rachelmarie001@aol.com>
2024-02-09 11:55:58 -0800

    database steps

--- flask-on-docker/services/web/manage.py
+++ flask-on-docker/services/web/manage.py
@@ -5,6 +5,11 @@ from project import app
 
 cli = FlaskGroup(app)
 
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == "__main__":
     cli()
